da7917e92c74c379de60c09bfb2c533ca94e0cff
Álvaro López <148997073+alvaro23LP@users.noreply.github.com>
2024-04-20 18:56:13 +0200

    Version 2 de ataque.dart

--- pr2/ataque.dart
+++ pr2/ataque.dart
@@ -2,33 +2,27 @@ import "militar.dart";
 
 abstract class Ataque{
   double danio = 0;
-  double desgaste = 0;
 
-  Ataque();
-  double atacar(Militar m){
+
+  void atacar(Militar m){
     m.recibeAtaque(danio);
-    return desgaste;
   }
 }
 
 class AtaqueAereo extends Ataque{
   AtaqueAereo(){
     danio=5;
-    desgaste=10;
   }
 }
 
 class AtaqueTerrrestre extends Ataque{
   AtaqueTerrrestre(){
     danio=1;
-    desgaste=8;
   }
 }
 
 class AtaqueMaritimo extends Ataque{
   AtaqueMaritimo(){
     danio=2;
-    desgaste=20;
   }
-
 }
